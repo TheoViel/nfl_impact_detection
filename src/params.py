ddba4c3a03b638fbbd9b317e3dba3764525baa8b
TheoViel <theo.viel@eleves.enpc.fr>
2021-01-09 21:58:42 +0100

    remove useless params

--- src/params.py
+++ src/params.py
@@ -6,8 +6,6 @@ NUM_WORKERS = 8
 DATA_PATH = "../input/"
 OUT_DIR = "../output/"
 
-LOG_PATH = "../logs/"
-LOG_PATH_CLS = "../logs_cls/"
 LOG_PATH_CLS_3D = "../logs_cls_3d/"
 
 SIZE = 512
@@ -19,7 +17,6 @@ IMG_PATH_F = "../../../data/nfl/"
 
 CROP_PATH = "../../../data/nfl_crops/"
 CROP_PATH_3D = "../../../data/nfl_crops_3d/"
-CROP_PATH_3D_2 = "../../../data/nfl_crops_3d_2/"
 
 MEAN = np.array([0.485, 0.456, 0.406])
 STD = np.array([0.229, 0.224, 0.225])
